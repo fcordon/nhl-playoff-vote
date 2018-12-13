d00bbc94577851347d96d6a1e27829710b667493
Fabien Cordon <f.cordon@cheil.com>
2018-12-13 15:35:40 +0100

    Complete form vote submit

--- src/containers/Vote.js
+++ src/containers/Vote.js
@@ -1,6 +1,6 @@
 // Lib
 import React, { Component } from 'react'
-import { Grid, Panel, Col, Form } from 'react-bootstrap'
+import { Grid, Panel, Col, Form, FormGroup, Checkbox, Button } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
@@ -9,7 +9,7 @@ import { getTeams } from "../actions/TeamsAction"
 
 // Components
 import nhlLogo from '../images/nhl.svg'
-import { VoteForm } from '../components/VoteForm'
+// import { VoteForm } from '../components/VoteForm'
 
 class Vote extends Component {
 
@@ -17,6 +17,32 @@ class Vote extends Component {
         super(props)
 
         this.props.getTeams()
+
+        this.state = {
+            'teamsSelected': []
+        }
+    }
+
+    onTeamChange(e) {
+        const name = e.target.name
+        const checked = e.target.checked
+        let teamsArray = [...this.state.teamsSelected]
+
+        if(checked)
+            teamsArray.push(name)
+        else {
+            let teamIndex = teamsArray.indexOf(name)
+            teamIndex !== -1 && teamsArray.splice(teamIndex, 1)
+        }
+
+        this.setState({
+            'teamsSelected': teamsArray
+        })
+    }
+
+    onFormSubmit(e) {
+        e.preventDefault()
+        console.log('Teams selected : ', this.state.teamsSelected)
     }
 
     render() {
@@ -28,8 +54,28 @@ class Vote extends Component {
                             <Panel.Title componentClass="h2"><img className='nhl-logo' src={nhlLogo} alt='NHL Logo' /> Teams</Panel.Title>
                         </Panel.Heading>
                         <Panel.Body>
-                            <Form>
-                                {this.props.teams.map((teams) => <VoteForm key={teams.id} {...teams} />)}
+                            <Form onSubmit={this.onFormSubmit.bind(this)}>
+                                {this.props.teams.map((teams, i) =>
+                                    <Col xs={6} md={4} lg={3} key={i}>
+                                        <FormGroup>
+                                            <label>
+                                                <img className='teams-logo' src={teams.franchise.teamName !== 'Golden Knights' ? 'https://www.nhl.com/site-core/images/team/logo/current/' + teams.id + '_dark.svg' : 'https://www-league.nhlstatic.com/images/logos/teams-current-primary-light/54.svg'} alt={teams.name} />
+                                                {teams.name}
+                                                <Checkbox
+                                                    className='form-checkbox'
+                                                    id={teams.id}
+                                                    title={teams.name}
+                                                    name={teams.name}
+                                                    onChange={this.onTeamChange.bind(this)}
+                                                    value={teams.name}
+                                                ></Checkbox>
+                                            </label>
+                                        </FormGroup>
+                                    </Col>
+                                )}
+                                <Col xs={7} xsOffset={5}>
+                                    <Button type="submit">Submit</Button>
+                                </Col>
                             </Form>
                         </Panel.Body>
                     </Panel>
