04da938cb376c22bd4f6bef46c3b80e7316e18e6
Fabien Cordon <f.cordon@cheil.com>
2019-03-08 15:42:08 +0100

    add /build to git

---
+++ client/build/precache-manifest.b7ff4a865cdd77ca6e802e54e70cafc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "266661c740be8a3de6af0532c93d514f",
+    "url": "/static/media/nhl.266661c7.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bec030590be24dc3e512",
+    "url": "/static/js/main.bec03059.chunk.js"
+  },
+  {
+    "revision": "9eca79e91a32e96a77ff",
+    "url": "/static/js/1.9eca79e9.chunk.js"
+  },
+  {
+    "revision": "bec030590be24dc3e512",
+    "url": "/static/css/main.2e1ce500.chunk.css"
+  },
+  {
+    "revision": "033a59ce36f3d8440f36643298472d09",
+    "url": "/index.html"
+  }
+];
