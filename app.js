bc0d37fdebf56c56482cd032fce9ab681410e1e1
Fabien Cordon <f.cordon@cheil.com>
2019-03-08 15:27:46 +0100

    Fix not found

--- app.js
+++ app.js
@@ -18,12 +18,12 @@ const users = require('./routes/user');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '/client/public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 // API
 const mongoose = require('mongoose');
@@ -63,6 +63,10 @@ main().catch(console.dir);
 app.use(passport.initialize());
 require('./passport')(passport);
 app.use('/', users);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+});
 
 //---->>>> GET TEAMS <<<<----
 app.get('/teams', function(req, res) {
