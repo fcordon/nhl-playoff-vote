6f035f14a4e1f6d820132f567f0f060ef0ca5638
Fabien Cordon <f.cordon@cheil.com>
2019-04-04 14:45:16 +0200

    Design user vote component

--- client/src/components/UserVote.js
+++ client/src/components/UserVote.js
@@ -2,37 +2,35 @@ import React, { Component } from 'react'
 import { Card, Row, Col } from 'react-bootstrap'
 
 // Components
-import nhlLogo from '../images/nhl.svg'
+import easternLogo from '../images/Eastern_Conference.png'
+import westernLogo from '../images/Western_Conference.png'
 
 export default class UserVote extends Component {
   render() {
     return (
-      <Col xs={12} md={6}>
+      <Col xs={12} md={6} lg={3}>
         <Card>
           <Card.Header>
             <Card.Title>
-              <img className='nhl-logo' src={nhlLogo} alt='NHL Logo' />
+              {this.props.match < 4 ? <img className='nhl-logo' src={easternLogo} alt='NHL Logo' /> : <img className='nhl-logo' src={westernLogo} alt='NHL Logo' />}
               Match {this.props.match + 1}
             </Card.Title>
           </Card.Header>
           <Card.Body>
-            <Row className='user-series-vote align-center'>
-              <Col xs={6} md={4} className={this.props.team1.score === 4 && 'user-series-vote-score font-bold'}>
+            <Row className='user-series-vote'>
+              <Col xs={4}>
                 <img src={this.props.team1.img} alt={this.props.team1.name} />
-                <p>{this.props.team1.name}</p>
               </Col>
-              <Col xs={3} md={1} className={this.props.team1.score === 4 ? 'user-series-vote-score font-bold' : 'user-series-vote-score'}>
-                {this.props.team1.score}
+              <Col xs={7} className={this.props.team1.score === 4 && 'user-series-vote-score font-bold'}>
+                <p>{this.props.team1.name} {this.props.team1.score}</p>
               </Col>
-              <Col xs={12} md={2} className='user-series-vote-score'>
-                VS
-              </Col>
-              <Col xs={3} md={1} className={this.props.team2.score === 4 ? 'user-series-vote-score font-bold' : 'user-series-vote-score'}>
-                {this.props.team2.score}
-              </Col>
-              <Col xs={6} md={4} className={this.props.team2.score === 4 && 'user-series-vote-score font-bold'}>
+            </Row>
+            <Row className='user-series-vote'>
+              <Col xs={4}>
                 <img src={this.props.team2.img} alt={this.props.team2.name} />
-                <p>{this.props.team2.name}</p>
+              </Col>
+              <Col xs={7} className={this.props.team2.score === 4 && 'user-series-vote-score font-bold'}>
+                <p>{this.props.team2.name} {this.props.team2.score}</p>
               </Col>
             </Row>
           </Card.Body>
