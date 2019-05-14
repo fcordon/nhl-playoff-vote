35be2a23eadfcf02019a9152e520959afced9794
Fabien Cordon <fcordon1@hotmail.fr>
2019-05-14 17:12:54 +0200

    Build 1.8

---
+++ client/build/precache-manifest.58108d1f608c31b9ede414c10acc8b4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c93d60833da9eb3857113f9d997fd3c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00740e36f69023f42197",
+    "url": "/static/css/main.31e6458b.chunk.css"
+  },
+  {
+    "revision": "73ac1e80cf615ed9d6bd",
+    "url": "/static/js/2.321c0e2d.chunk.js"
+  },
+  {
+    "revision": "00740e36f69023f42197",
+    "url": "/static/js/main.5fb3ae10.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fdd85eae1bf247a6e7438f5e1e57a464",
+    "url": "/static/media/Eastern_Conference.fdd85eae.png"
+  },
+  {
+    "revision": "223abb5ad8f4c923d7f49fae091a53ac",
+    "url": "/static/media/Western_Conference.223abb5a.png"
+  },
+  {
+    "revision": "266661c740be8a3de6af0532c93d514f",
+    "url": "/static/media/nhl.266661c7.svg"
+  }
+]);
