a503ac24d9a5b1884d6420c4f0d4667022b6542a
Fabien Cordon <fcordon1@hotmail.fr>
2019-05-14 17:16:19 +0200

    Build 2.8

---
+++ client/build/precache-manifest.7f86e815323bb3c275310a5bc1d8c871.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d955f1c6e7be3d132f8b20f5a369924",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f9eee474de8e64ff129",
+    "url": "/static/css/main.e4e86097.chunk.css"
+  },
+  {
+    "revision": "7aa9381051e1c18b2608",
+    "url": "/static/js/2.e688e1fe.chunk.js"
+  },
+  {
+    "revision": "7f9eee474de8e64ff129",
+    "url": "/static/js/main.b4bba6b5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fdd85eae1bf247a6e7438f5e1e57a464",
+    "url": "/static/media/Eastern_Conference.fdd85eae.png"
+  },
+  {
+    "revision": "223abb5ad8f4c923d7f49fae091a53ac",
+    "url": "/static/media/Western_Conference.223abb5a.png"
+  },
+  {
+    "revision": "266661c740be8a3de6af0532c93d514f",
+    "url": "/static/media/nhl.266661c7.svg"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94fd5b97c330f0ed439513789a2e2561.js"
+  "/precache-manifest.7f86e815323bb3c275310a5bc1d8c871.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
